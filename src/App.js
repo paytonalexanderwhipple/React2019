111a6432ca505e1e8e407baf177e42ae645ff08d
Payton Whipple <web-dev@paytonwhipple.com>
2019-07-16 13:26:06 -0600

    Added Header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/Homepage/Homepage.jsx';
 import Shop from './pages/Shop/Shop.jsx';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/shop" component={Shop} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
+    </>
   );
 }
 
